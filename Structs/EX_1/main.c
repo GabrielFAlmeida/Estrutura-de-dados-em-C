eb91f32da2c848d1b3598685ae3862df88a6d4b0
Gabriel França de Almeida <54248551+GabrielFAlmeida@users.noreply.github.com>
2021-01-07 16:37:19 -0300

    Update main.c

--- Structs/EX_1/main.c
+++ Structs/EX_1/main.c
@@ -1,5 +1,4 @@
-/*
-*/
+/* Crie uma struct "Aluno" que contenha o seu nome, seu curso e o nÃºmero de sua matrÃ­cula. Posteriormente implemente a struct "tam" vezes e imprima os seus conteÃºdos.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
@@ -21,9 +20,9 @@ int main()
     {
         printf("Digite o nome do %i aluno..: \n",i+1);
         gets(a[i].nome);
-        printf("Digite o número de matrícula do %i° aluno..: \n",i+1);
+        printf("Digite o nÃºmero de matrÃ­cula do %iÂ° aluno..: \n",i+1);
         gets(a[i].n_matricula);
-        printf("Digite o curso do %i° aluno..: \n",i+1);
+        printf("Digite o curso do %iÂ° aluno..: \n",i+1);
         gets(a[i].curso);
         system("cls");
     }
@@ -32,7 +31,7 @@ int main()
     for(i=0; i<tam; i++)
     {
         printf("||  Nome[%i] = %s   ||", i+1 , a[i].nome);
-        printf("||  Número de matrícula[%i] = %s  ||", i+1, a[i].n_matricula);
+        printf("||  NÃºmero de matrÃ­cula[%i] = %s  ||", i+1, a[i].n_matricula);
         printf("||  Curso[%i] = %s  ||\t",i+1, a[i].curso);
         printf("\n");
     }
